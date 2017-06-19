6a8d03d596de8d4f74fc985c45dcb389387165a2
malacourse <malacourse>
2017-06-19 13:27:41 -0400

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,18 @@
-import pod_bot
-
+from pod_bot import PodBot
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def status_page():
-    print ("Start status page")
+    print ("<h>Pod Status Page</h>")
+    print ("<h2><a href='/config'>Configuration</a></h2>")
     bot = PodBot()
     bot.about()
     return bot.about()
+
+@application.route("/config")
+def config_page():
+    println ("OpenShift URL: " + os.environ["OPENSHIFT_URL"])
 
 if __name__ == "__main__":
     application.run()
