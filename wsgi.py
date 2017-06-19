8e9ded6fe9b0438592d881a4dba3db718987cbad
malacourse <malacourse>
2017-06-19 13:40:57 -0400

    fix

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,15 @@ application = Flask(__name__)
 
 @application.route("/")
 def status_page():
-    print ("<h>Pod Status Page</h>")
-    print ("<h2><a href='/config'>Configuration</a></h2>")
+    def retStr =  "<h>Pod Status Page</h>"
+    retStr +=  "<h2><a href='/config'>Configuration</a></h2>"
     bot = PodBot()
     bot.about()
-    return bot.about()
+    return retStr
 
 @application.route("/config")
 def config_page():
-    println ("OpenShift URL: " + os.environ["OPENSHIFT_URL"])
+    return "OpenShift URL: " + os.environ["OPENSHIFT_URL"]
 
 if __name__ == "__main__":
     application.run()
