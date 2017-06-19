9e70f1c3c442144d737eeaa16bc762eb20edcf20
malacourse <malacourse>
2017-06-19 09:38:31 -0400

    revert

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "This is the Pod watcher server!"
+    return "Hello World!"
 
 if __name__ == "__main__":
-    application.run(port=8070)
+    application.run()
