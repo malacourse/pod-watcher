2c76178f5d9a57360bc8a61b0e3657bf862342b5
malacourse <malacourse>
2017-06-19 10:04:18 -0400

    add podbot

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,10 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    print "start hello"
+    bot = PodBot()
+    bot.about()
+    return "Hello New World!"
 
 if __name__ == "__main__":
     application.run()
