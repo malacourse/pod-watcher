78902f5bb315e9244ac0085899a7763f5c397e45
malacourse <malacourse>
2017-06-20 16:08:11 -0400

    fix

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+import PodBot from pod_bot
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+botstatus = PodBot().get_status()
