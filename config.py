86bc8498be48f82577348fc4769185e610257f6c
malacourse <malacourse>
2017-06-21 09:16:29 -0400

    fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 print("Config Starting")
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
